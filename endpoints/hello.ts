e2d41adb05fae5357145daf72ac5ea050d1e45ef
luisfvieirasilva <luisfvieirasilva@gmail.com>
2022-06-15 14:57:54 -0300

    Update hello

--- endpoints/hello.ts
+++ endpoints/hello.ts
@@ -5,7 +5,7 @@ type Handler = (req: Request, res: Response) => Response | Promise<Response>
 
 const handleGet: Handler = async req => {
 // hello
-  return responseFromJson({hello: "world github"})
+  return responseFromJson({hello: "Hello there"})
 }
 
 const handlers: Record<string, Handler> = {
